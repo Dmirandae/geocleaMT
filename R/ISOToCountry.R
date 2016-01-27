5837a3e2a549a52bece0c4234e6a38ee8030fc5d
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2016-01-26 19:44:03 -0500

    ISOToCountry Updated

--- R/ISOToCountry.R
+++ R/ISOToCountry.R
@@ -1,19 +1,17 @@
 #'@name ISOTocountry
 #'
-#'@title Convert from  International Organization for Standardization (ISO 3166-1 alpha-2 and  alpha-3)
+#'@title Convert from the International Organization for Standardization (ISO 3166-1 alpha-2 or alpha-3) code
 #' to country name (English).
 #'
-#'@description Given the ISO-2 or ISO-3 code [1], it  will assign the corresponding country name in English.
+#'@description Given the lenght of the ISO country code provided (2=ISO-2 or 3=ISO-3[1]), the function will assign the corresponding country name in English.
 #'
 #'@param countryCode Data.Frame class. A vector with the ISO-2or ISO-3 Codes.
-#'
-#'@param iso numeric (2 or 3). To define the code whether ISO-2 (iso=2, default value) or ISO-3 (iso=3).
 #'
 #'
 #'@return A table with two columns (countryCode, country)
 #'
 #'
-#'@details The user can see the ISO 3166-1 alpha-2 or alpha-3 valid for each country using
+#'@details The user can check the ISO 3166-1 alpha-2 or alpha-3 code, valid for each country using
 #' the line of code : data(ISO2) or data(ISO3).
 #'
 #'@author R-Alarcon Viviana and Miranda-Esquivel Daniel R.
@@ -28,15 +26,19 @@
 #'[1] ISO 2015. International standard for country codes and codes for their subdivisions. Country codes - iso 3166. International Organization for Standardization. Available online at http://www.iso.org/iso/country_codes .
 
 
-ISOTocountry <- function(countryCode = NULL, iso = 2) {
+ISOTocountry <- function(countryCode = NULL) {
 
-	if(iso!=2 | iso!=3){stop('Wrong ISO value')}
-	
+  iso <- nchar(countryCode)
+
+  if (all(iso != c(2,3))) {
+    stop("Wrong ISO length")
+    }
+
     data(wrld_simpl)
     world <-wrld_simpl
-    
+
     table <- cbind('countryCode' = countryCode, 'country' = NaN)
-    
+
   for (i in 1:length(countryCode)) {
     table$countryCode[i] <- countryCode[i]
     if(iso==2){position <- which(world@data$ISO2 == countryCode[i])}
